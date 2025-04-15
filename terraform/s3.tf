69f0391c00aa1b8975e471ee7b20352e29e495e5
Prashant <113302801+prashantsingh34@users.noreply.github.com>
2025-04-15 10:03:15 +0530

    moved event bridge related code to a seperate file

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -41,37 +41,6 @@ resource "aws_s3_bucket_notification" "eventbridge_enable" {
   eventbridge = true
 }
 
-resource "aws_cloudwatch_event_bus" "file_transfer_event_bus" {
-  name = "file-process-event-bus"
-}
-
-resource "aws_cloudwatch_event_rule" "s3_object_created" {
-  name        = "s3-object-created-event"
-  event_bus_name = aws_cloudwatch_event_bus.file_transfer_event_bus.name
-  description = "Trigger Step Function on S3 object upload"
-  event_pattern = jsonencode({
-    "source": ["aws.s3"],
-    "detail-type": ["Object Created"],
-    "detail": {
-      "bucket": {
-        "name": [aws_s3_bucket.file_to_be_processed.bucket]
-      },
-      "object": {
-        "key": [{ "prefix": "uploads/" }]
-      }
-    }
-  })
-  depends_on=[aws_cloudwatch_event_bus.file_transfer_event_bus]
-}
-
-resource "aws_cloudwatch_event_target" "start_step_function" {
-  rule      = aws_cloudwatch_event_rule.s3_object_created.name
-  event_bus_name = aws_cloudwatch_event_bus.file_transfer_event_bus.name
-  target_id = "StartStepFunction"
-  arn       = aws_sfn_state_machine.s3_event_triggered.arn
-  role_arn  = aws_iam_role.eventbridge_invoke_stepfn_role.arn
-  depends_on=[aws_cloudwatch_event_bus.file_transfer_event_bus]
-}
 
 ## processed bucket
 
